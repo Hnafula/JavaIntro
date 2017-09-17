5b0c1388584080d370834be77c94faabe24e7e9e
Hnafula <hildasichangi@gmail.com>
2017-09-17 16:58:16 +0300

    created add and multiply functions

---
+++ Example.java
@@ -0,0 +1,32 @@
+public class Example{
+
+public static void main(String[] args){
+
+
+String str="one";
+
+String srt1="two";
+
+int x=5;
+int y=10;
+x=y+x+5+7;
+int sum=0;
+sum =add(x,y);
+
+System.out.println("we are learning java today");
+System.out.println("sum##"+sum);
+
+}
+
+public static int add(int a,int b){
+return a+b;
+	
+}
+
+public static int multiply(int a, int b){
+    return (a*b);
+}
+
+
+
+}
